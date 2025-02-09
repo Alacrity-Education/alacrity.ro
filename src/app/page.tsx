68b091a7f095c084b10f990f6298968f1eba7828
Ionescu Andrei <andreiionescu750@gmail.com>
2025-02-09 19:04:24 +0200

    gallery landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Hero from "../components/ui/hero";
 import About from "@/components/ui/about";
 import Team from "@/components/ui/team";
 import AirBox from "@/components/ui/airbox";
+import Gallery from "@/components/ui/gallery";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Gallery />
       <Showcase />
       <AirBox />
       <Team />
---
+++ src/components/ui/gallery.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import Image from "next/image";
+
+const Gallery = () => {
+  return (
+    <div className="relative h-max bg-base-200 flex flex-col items-center py-10 pb-20">
+      <div className="text-2xl sm:text-5xl font-semibold py-16">
+        <span className="text-white">Visual </span>
+        <span className="text-purple-500">Gallery</span>
+      </div>
+
+      <div className="w-[100vw] mx-auto z-10 relative px-4">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 relative z-10">
+          {/* Left Column */}
+          <div className="flex flex-col gap-6">
+          <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/left1.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+            <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/left2.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+            <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/left3.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+          </div>
+
+          {/* Middle Column */}
+          <div className="flex flex-col gap-6">
+            <div className="h-[calc(50%-0.75rem)] rounded-lg relative w-full">
+            <Image
+                alt="bg"
+                src="/galerie/middle1.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+            <div className="h-[calc(50%-0.75rem)] rounded-lg relative w-full">
+            <Image
+                alt="bg"
+                src="/galerie/middle2.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+          </div>
+
+          {/* Right Column */}
+          <div className="flex flex-col gap-6">
+            
+            <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/right1.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+            <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/right2.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+            <div className="aspect-video rounded-lg relative w-full">
+              <Image
+                alt="bg"
+                src="/galerie/right3.jpg"
+                layout="fill"
+                objectFit="cover"
+                className="absolute w-full h-full left-0 top-0 scale-100 mix-blend-overlay dark:mix-blend-normal rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="max absolute inset-0 flex justify-center items-center z-1 hidden sm:flex">
+          <svg
+            className="w-full h-auto"
+            viewBox="0 0 1440 800"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1470.6 360C1282.95 360 1282.95 440 1095.3 440C907.65 440 907.65 360 720 360C532.35 360 532.35 440 344.7 440C157.05 440 157.05 360 -30.6 360"
+              stroke="#50247C"
+              strokeWidth="80"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M-30.6 120C157.05 120 157.05 200 344.7 200C532.35 200 532.35 120 720 120C907.65 120 908.484 200 1096.13 200C1283.78 200 1282.95 120 1470.6 120"
+              stroke="#50247C"
+              strokeWidth="80"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M1470.6 600C1282.95 600 1282.95 680 1095.3 680C907.65 680 907.65 600 720 600C532.35 600 532.35 680 344.7 680C157.05 680 157.05 600 -30.6 600"
+              stroke="#50247C"
+              strokeWidth="80"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
