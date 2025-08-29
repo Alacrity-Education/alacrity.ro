3a151b19f03e952ef4526b22433abf5e454ec169
LucaSain <lucasainenco2@gmail.com>
2025-08-29 13:07:51 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'googleusercontent.com',
+        pathname: '/profile/picture/0**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'googleusercontent.com',
+        pathname: '/profile/picture/1**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'script.google.com',
+        pathname: '/**', // This will allow any path from this host
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
