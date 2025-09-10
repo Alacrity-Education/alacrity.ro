c2cef402a0b42ed119006bb3969cd7724969981b
terrablade <terrablade@terrablades-MacBook-Pro.local>
2025-09-10 23:12:25 +0300

    revert to non-api members

--- src/components/ui/team.tsx
+++ src/components/ui/team.tsx
@@ -3,17 +3,67 @@ import { Suspense } from "react";
 
 interface TeamMember {
   name: string;
-  title: string;
-  photoUrl?: string;
-  id:number;
+  role: string;
+  image?: string;
 }
 
 
+  const members: TeamMember[] = [
+    {
+      name: "Vavilov Iris",
+      role: "Founder and CEO",
+      image: "/team/iris.jpg",
+    },
+    {
+      name: "Heroiu Justinian",
+      role: "Founder and Research lead.",
+      image: "/team/justin.png",
+    },
+    {
+      name: "Alexa Gabriela",
+      role: "Founder and COO",
+      image: "/team/tina.jpg",
+    },
+    {
+      name: "Sainenco Luchian",
+      role: "Founder and CTO",
+      image: "/team/luca.png",
+    },
+    {
+      name: "Mușel Răzvan",
+      role: "Project Manager",
+      image: "/team/musel.jpg",
+    },
+    {
+      name: "Ionescu Andrei",
+      role: "Software Developer",
+      image: "/team/stefan.jpg",
+    },
+    {
+      name: "Druică Adina",
+      role: "Graphic Designer",
+      image: "/team/adina.jpg",
+    },
+    {
+      name: "Sima Bianca",
+      role: "Student Researcher",
+      image: "/team/bianca.png",
+    },
+    {
+      name: "Tudor Zgîmbău",
+      role: "Manager & economist",
+      image: "/team/tudor.jpeg",
+    },
+    {
+      name: "Stoica Mihai",
+      role: "Trainer",
+      image: "/team/mihai.jpg",
+    },
+  ];
 
 
 export default async function Team() {
 
-  const members: TeamMember[] = ((await fetch(process.env.WORKSPACE_MEMBERS_URL || "",{ next: { revalidate: 600 } }).then(res => res.json())).users as TeamMember[])
 
 
   return (
@@ -22,10 +72,7 @@ export default async function Team() {
         Meet the team behind <span className="text-primary">Alacrity</span>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-rows-2">
-         {members.filter((member)=> member.title && member.title.includes("Founder")).map((member, index) => (
-          <TeamMember key={index} {...member} />
-        ))}
-        {members.filter((member)=> !member.title || !member.title.includes("Founder")).map((member, index) => (
+        {members.map((member, index) => (
           <TeamMember key={index} {...member} />
         ))}
       </div>
@@ -33,17 +80,8 @@ export default async function Team() {
   );
 }
 
-async function getUserPhoto(userId: number | string) {
-  const res = await fetch(`${process.env.WORKSPACE_MEMBERS_URL}?photoUser=${userId}`,{next:{revalidate:600}});
-  if (!res.ok) return '/Falcon.svg'; // Fallback to a static image if error
-  const { data, contentType } = await res.json();
-  if(!data)
-    return "/Falcon.svg"
-  return `data:${contentType};base64,${data}`;
-}
 
-const TeamMember = async ({id, name, title, photoUrl }: TeamMember) => {
-  const imageUrl = await getUserPhoto(id)
+const TeamMember = async ({name, role, image }: TeamMember) => {
   return (
  
     <div className="h-64 w-40 md:h-64 md:w-64 p-4 flex flex-col gap-2 items-center justify-center">
@@ -52,7 +90,7 @@ const TeamMember = async ({id, name, title, photoUrl }: TeamMember) => {
 
           <Image
             loading="lazy"
-            src={imageUrl || "/Falcon.svg"}
+            src={image || "/Falcon.svg"}
             height={1000}
             width={1000}
             alt={name}
@@ -64,7 +102,7 @@ const TeamMember = async ({id, name, title, photoUrl }: TeamMember) => {
         </div>
       </div>
       <div className="text-sm sm:text-md font-bold text-center">{name}</div>
-      <div className="text-xs sm:text-sm">{title || "volunteer"}</div>
+      <div className="text-xs sm:text-sm">{role || "volunteer"}</div>
     </div>
    
   );
