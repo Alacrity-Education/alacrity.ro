8349e80f034847251db4ec602b65eb17b239cdf1
LucaSain <lucasainenco2@gmail.com>
2025-11-18 09:30:44 +0200

    improved readiness on main hero

--- src/app/components/ui/homeHero.tsx
+++ src/app/components/ui/homeHero.tsx
@@ -19,29 +19,27 @@ const HeroContent = () => {
       />
 
       <HeroText>
-        <h1 className="text-3xl md:text-3xl lg:text-5xl font-bold text-center text-base-content dark:text-white sm:text-start sm:pt-4">
+        <h1 className="text-3xl md:text-3xl lg:text-5xl tracking-wide font-bold text-center text-base-content/90  sm:text-start sm:pt-4">
           Improving the education <br />
-          <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary/90 to-secondary/90">
             of the future
           </span>
         </h1>
-        <p className="py-10 sm:py-6  md:text-xl text-base-content md:mr-20">
+        <p className="py-10 sm:py-6 text-base md:text-xl text-base-content md:mr-20">
           We’re opening Romania’s first{" "}
           <b className="text-primary">open-source STEAM</b> lab, where{" "}
           <b className="text-primary">education </b>
           meets <b className="text-primary">invention</b>.
           <br />
+          <br />
           From air quality sensors to environmental tech, we help young minds
           shape a{" "}
-          <b className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
-            sustainable and intelligent future
-          </b>
-          .
+          <b className="text-primary">sustainable and intelligent future</b>.
         </p>
 
         <a
           href="https://buymeacoffee.com/alacrity_education?ref=alacrity.education"
-          className="btn bg-gradient-to-tr from-secondary text-white to-primary border-0 btn-lg hover:-translate-y-1 duration-500 transition-transform z-50 dark:shadow-xl  "
+          className="btn btn-primary border-0 btn-lg hover:-translate-y-1 duration-500 transition-transform z-50 shadow-xl hover:shadow-2xl "
         >
           Donate for our lab!
         </a>
