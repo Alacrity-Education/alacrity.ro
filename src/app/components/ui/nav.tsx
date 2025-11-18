c6a7e0f20917bb5df0c1c4fd11ac676182dcbe78
LucaSain <lucasainenco2@gmail.com>
2025-11-18 09:47:15 +0200

    updated website navbar

--- src/app/components/ui/nav.tsx
+++ src/app/components/ui/nav.tsx
@@ -3,22 +3,101 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="navbar fixed bg-base-200 shadow-sm px-2 py-2 z-50">
-      <div className="flex-1 h-max">
-        <Link
-          href="/"
-          className="btn btn-ghost font-bold lg:text-xl text-primary"
-        >
-          Alacrity Education
-        </Link>
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu dropdown-content bg-base-300 rounded-box z-1 mt-5 w-52 p-2 shadow"
+          >
+            <li>
+              <details>
+                <summary>Projects</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Airbox</a>
+                  </li>
+                  <li>
+                    <a>Raven1 Science Fair</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <details>
+                <summary>Raven1</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Landing</a>
+                  </li>
+                  <li>
+                    <a>About</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <Link href="https://blog.alacrity.ro"> Alacrity Blog</Link>
+            </li>
+            <li>
+              <Link href="/about" className="">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Alacrity Education</a>
       </div>
-      <div className="flex-none flex gap-2">
-        <a href="https://blog.alacrity.ro" className="btn btn-ghost  ">
-          Blog
-        </a>
-        <Link href="/about" className="btn btn-ghost ">
-          About
-        </Link>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 z-10">
+          <li className="">
+            <details>
+              <summary>Projects</summary>
+              <ul className="p-2 bg-base-300 w-52">
+                <li>
+                  <a>Airbox</a>
+                </li>
+                <li>
+                  <a>Raven1 Science Fair</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <Link href="https://blog.alacrity.ro" className="">
+              RAVEN1 Robotics
+            </Link>
+          </li>
+          <li>
+            <Link href="https://blog.alacrity.ro" className=" ">
+              Alacrity Blog
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="">
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
         <a
           href="https://buymeacoffee.com/alacrity_education?status=1"
           className="btn btn-primary"
