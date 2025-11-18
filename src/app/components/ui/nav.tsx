f5ec8e680ee3ed9a1902845d9bc81340ddcb36df
LucaSain <lucasainenco2@gmail.com>
2025-11-18 09:50:57 +0200

    make the navbar fixed for ux

--- src/app/components/ui/nav.tsx
+++ src/app/components/ui/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-300 shadow-sm">
+    <div className="navbar bg-base-100 dark:bg-base-300 shadow-sm fixed top-0 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
